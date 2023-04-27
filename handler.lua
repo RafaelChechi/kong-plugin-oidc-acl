3a1021331c3272bb4024be916212b6a97b6f493c
Rafael Chechi <rafael.chechi@compasso.com.br>
2023-04-26 22:01:31 -0300

    Plugin ACL

--- handler.lua
+++ handler.lua
@@ -2,21 +2,23 @@ local ACL = {
     PRIORITY = 950,
     VERSION = "1.0-3",
 }
+
 local cjson = require("cjson")
 
 function ACL:access(plugin_conf)
     local whitelist = plugin_conf.whitelist
-    local userroles = get_user_roles(plugin_conf.userinfo_header_name)
+    local userroles = get_user_roles()
 
     if has_value(whitelist, userroles) then
         return
     else
-        return kong.response.exit(403, {
-            message = "You cannot consume this service"
-        })
+        ngx.status = 401
+        ngx.say("You cannot consume this service")
+        ngx.exit(ngx.HTTP_UNAUTHORIZED)
     end
 
 end
+
 
 function has_value (tab, val)
     for _, value in ipairs(tab) do
@@ -30,25 +32,27 @@ function has_value (tab, val)
     return false
 end
 
+
 function mysplit(inputstr, sep)
     if sep == nil then
         sep = "%s"
     end
-    local t = {};
-    local i = 1
-    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+    local t={} ;
+    local i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
         t[i] = str
         i = i + 1
     end
     return t
 end
 
-function get_user_roles(userinfo_header_name)
+
+function get_user_roles()
     local h = ngx.req.get_headers()
     for k, v in pairs(h) do
-        if string.lower(k) == string.lower(userinfo_header_name) then
-            local user_info = cjson.decode(ngx.decode_base64(v))
-            local roles = table.concat(user_info["realm_access"]["roles"], ",")
+        if k == 'x-userinfo' then
+            local user = cjson.decode(ngx.decode_base64(v))
+            local roles = table.concat(user["realm_access"]["roles"],",")
             return mysplit(roles, ",")
         end
     end
